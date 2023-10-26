f4413da79565a9b34dc8399a9769a03d115a8162
MauricioNC <mauricionc19@gmail.com>
2023-10-26 16:05:52 -0600

    feat: Search form added in the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def home
   end
+
+  def search
+    @query = params[:q]
+    @posts = Post.all.map{ |post| post if post.tags.select { |t| t === params[:q] } }
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.turbo_stream
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#home'
-  get '/profile/:username', to: 'users#profile' 
+  get '/profile/:username', to: 'users#profile'
+  post '/search', to: 'home#search'
 
   resources :users
 end
