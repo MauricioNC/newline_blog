cf9d1897acd0333aef7ed8dd53d1b32ee40b4c93
MauricioNC <mauricionc19@gmail.com>
2023-10-26 07:11:42 -0600

    feat: Comment model created and associations between user, posts and comments established

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_26_125939) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_130525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "tags", default: [], null: false, array: true
@@ -41,5 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_26_125939) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
