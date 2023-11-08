4c14b86503611d76bad115444f1ffc7c5a0d1934
MauricioNC <mauricionc19@gmail.com>
2023-11-07 20:24:06 -0600

    feat: Login functionality completed

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Email or password incorrect"
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#home'
+  get '/sessions/login', to: 'sessions#new'
+  post '/sessions/login', to: 'sessions#create'
   get '/profile/:username', to: 'users#profile'
   post '/search', to: 'home#search'
   get '/:username/:post_title', to: 'posts#show'
