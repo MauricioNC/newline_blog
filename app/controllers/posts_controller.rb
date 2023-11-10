4a3122d825fd006922db1956a1c177fe6063c4ab
MauricioNC <mauricionc19@gmail.com>
2023-11-09 21:03:19 -0600

    feat: Added authenticate validation when an unnloged in user tries to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,17 @@
 class PostsController < ApplicationController
-  before_action :set_user
+  before_action :authenticate!, only: [ :new, :create ]
+  before_action :set_user, only: [ :show ]
 
   def show
     @post = @user.posts.where(title: params[:post_title])
     @tags = Tag.limit(5).order("RANDOM()")
     @posts = Post.limit(6).order("RANDOM()")
+  end
+
+  def new
+  end
+
+  def create
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   get '/profile/:username', to: 'users#profile'
   post '/search', to: 'home#search'
   get '/:username/:post_title', to: 'posts#show'
+  get '/posts/:username/new', to: 'posts#new'
 
   resources :users
-  resources :posts, except: [:show]
+  resources :posts, except: [:show, :new]
 end
