a0faddd8a272547cb925d6bedb1e9c569c55da3b
MauricioNC <mauricionc19@gmail.com>
2023-11-13 22:09:20 -0600

    fix: Deleted specific route to create a new post.

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,8 @@ Rails.application.routes.draw do
 
   get '/profile/:username', to: 'users#profile'
   post '/search', to: 'home#search'
-  get '/:username/:post_title', to: 'posts#show'
-  get '/posts/:username/new', to: 'posts#new'
+  get 'posts/:username/:post_title', to: 'posts#show'
 
   resources :users
-  resources :posts, except: [:show, :new]
+  resources :posts, except: [:show]
 end
