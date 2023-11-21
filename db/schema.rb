92d9ccd255c6a9e3082043e66ecdddc12f014b89
MauricioNC <mauricionc19@gmail.com>
2023-11-20 19:20:27 -0600

    fix: Tag field removed from post
    Tag field was removed from the posts table. Instead, was a created an
    extra table called post_tags, that is a relation between the posts
    table and the tags table, by this way, we only pass the tag_ids: []
    parameter when a post is created and the tags realted are saved into
    the post_tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_11_040616) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_17_022519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,8 +60,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_11_040616) do
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
+  create_table "post_tags", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
+  end
+
   create_table "posts", force: :cascade do |t|
-    t.string "tags", default: [], null: false, array: true
     t.string "raiting"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -91,5 +99,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_11_040616) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
+  add_foreign_key "post_tags", "posts"
+  add_foreign_key "post_tags", "tags"
   add_foreign_key "posts", "users"
 end
