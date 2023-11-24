1b6679b9ff2b48bec94957fb7df88092e0dd3554
MauricioNC <mauricionc19@gmail.com>
2023-11-23 22:35:25 -0600

    fix: Changed DOM load to turbo:load

--- app/javascript/highlight/index.js
+++ app/javascript/highlight/index.js
@@ -2,7 +2,7 @@ import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/es/
 
 hljs.configure({languages: ['ruby', 'bash', 'javascript', 'erb']})
 
-document.addEventListener("DOMContentLoaded", (e) => {
+document.addEventListener("turbo:load", (e) => {
   document.querySelectorAll("pre").forEach((el) => {
     hljs.highlightElement(el)
   })
