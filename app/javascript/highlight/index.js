57c90329ed09f542c2d25f76eebf7f1365de8f6e
MauricioNC <mauricionc19@gmail.com>
2023-11-24 17:17:27 -0600

    fix: Fixed issue with unescaped HTML that came from the highlight.js library

--- app/javascript/highlight/index.js
+++ app/javascript/highlight/index.js
@@ -1,9 +1,12 @@
 import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/es/highlight.min.js';
 
-hljs.configure({languages: ['ruby', 'bash', 'javascript', 'erb']})
-
 document.addEventListener("turbo:load", (e) => {
   document.querySelectorAll("pre").forEach((el) => {
-    hljs.highlightElement(el)
+    const codeTag = document.createElement("code")
+
+    // All the content in the pre tag, is passed to the code tag, then the code tag with the whole content is appended back to the pre tag
+    codeTag.textContent = el.innerHTML
+    el.innerHTML = ""
+    hljs.highlightElement(el.appendChild(codeTag))
   })
 })
