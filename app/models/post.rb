bebfa260a98063b64a071cb4c435e482e4455f72
MauricioNC <mauricionc19@gmail.com>
2023-12-21 05:35:23 -0600

    feat: Add dependent destroy constrain to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
-  has_many_attached :images
-  has_one_attached :banner
+  has_many_attached :images, dependent: :destroy
+  has_one_attached :banner, dependent: :destroy
   has_rich_text :content
 
   belongs_to :user
-  has_many :comments
-  has_many :post_tags
-  has_many :tags, through: :post_tags
+  has_many :comments, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :posts
-  has_many :comments, through: :posts
+  has_many :posts, dependent: :destroy
+  has_many :comments, through: :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
