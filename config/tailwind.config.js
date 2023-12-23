acc173e8cf6018e2367bf6fac864597d5a3a8d1f
MauricioNC <mauricionc19@gmail.com>
2023-12-23 15:10:40 -0600

    feat: Top menu styles
    - Styles for top menu header completed
    - Background applied to the body
    - Box shadow and colors created in tailwind.config.js file

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,15 +8,35 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    screens: {
+      'xs': '320px',
+      ...defaultTheme.screens
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        montserrat: 'Montserrat'
       },
       height: {
         'bannerH': '450px'
       },
       boxShadow: {
         'tagsShadow': '#076678'
+      },
+      colors: {
+        'gv-red-orange': '#FB4934',
+        'gv-dark': '#1D2021',
+        'gv-oasis': '#FBF1C7',
+        'gv-smalt-blue': '#458588',
+        'gv-tulip-pink': '#B16286',
+        'gv-grey-green': '#689D6A',
+        'gv-dark-aqua': '#076678',
+        'gv-pistachio': '#8EC07C',
+        'gv-bamboo': '#D65D0E',
+        'gv-fire': '#AF3A03',
+      },
+      boxShadow: {
+        'hard': '4px 4px 0 0 #000'
       }
     },
   },
