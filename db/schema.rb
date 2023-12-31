714885aa020283eb8bc6e389076a0a8469d4194f
MauricioNC <mauricionc19@gmail.com>
2023-12-30 23:05:42 -0600

    feat: Removed description field from Posts model. Unnecessary field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_043659) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_31_050056) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,7 +83,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_043659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title", null: false
-    t.string "description", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
