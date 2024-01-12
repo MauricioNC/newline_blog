b61638cc8b0c610a6f647c5c87f848892a195c82
MauricioNC <mauricionc19@gmail.com>
2024-01-11 21:42:10 -0600

    feat: Tags styles completed

--- app/javascript/controllers/tags_controller.js
+++ app/javascript/controllers/tags_controller.js
@@ -2,55 +2,37 @@ import { Controller } from "@hotwired/stimulus"
 import { FetchRequest } from '@rails/request.js'
 
 // Connects to data-controller="tags"
+let tagsAutoComplete = ""
+let listOfTags = ""
 export default class extends Controller {
   connect() {
-    this.url = "http://localhost:3000/tags"
-    this.method = "GET"
-    this.securityToken = document.head.querySelector("meta[name=csrf-token]")?.content
+    // AJAX request parameters
+    this.url = "http://localhost:3000/tags" // url for AJAX request
+    this.method = "GET" // GET method for AJAX request
+    this.securityToken = document.head.querySelector("meta[name=csrf-token]")?.content // CSFR token
+    
     this.comboboxTag = document.querySelector(".combobox-tags")
     this.tagsSelected = document.querySelector("#tags-selected")
-    this.comboboxSearch = document.querySelector("#combobox-search")
-    this.inputSearch = ""
-    this.closeSpan = `<span class='px-4 py-2 bg-neutral-900 rounded-tr-lg rounded-br-lg font-bold' data-controller='tags' data-action='click->tags#removeTag'>X</span>`
+    this.comboboxSearch = document.querySelector("#combobox-search") 
+    this.closeSpan = `<span class='px-4 py-2 bg-neutral-900 rounded-tr-lg rounded-br-lg font-bold' data-controller='tags' data-action='click->tags#removeTagItem'>X</span>`  
   }
 
   async listTags() {
-    const entryValue = this.element.value
+    this.createAutoCopmpleteElement()
+    this.creatListOfTagsElement()
+    
     const tagsList = await this.getTagsLits()
-    const div = document.createElement("div")
-    const ul = document.createElement("ul")
-
-    this.inputSearch = entryValue
-
-    const filteredTags = tagsList.filter((tag) => tag.includes(entryValue) ? tag : '')
-
+    const filteredTags = tagsList.filter((tag) => tag.includes(this.element.value) ? tag : '')
+    
     if (filteredTags.length > 0) {
-      for (let i = 0; i < filteredTags.length; i++) {
-        const li = `<li id='${filteredTags[i]}' class='border-b-2 border-b-gv-baltic-sea p-2'} data-controller='tags' data-action='click->tags#appendToCombobox'>
-          ${filteredTags[i]}
-        </li>`
-        ul.insertAdjacentHTML("beforeend", li)
-      }
-      div.id = "tags-autocomplete"
-      div.classList.add("bg-gv-dark-jungle", "p-2", "mt-2")
-      ul.classList.add("list-of-tags")
-      div.appendChild(ul)
-      
-      if (document.querySelector("#tags-autocomplete")) {
-        this.comboboxTag.replaceChild(div, document.querySelector("#tags-autocomplete"))
-      } else {
-        this.comboboxTag.appendChild(div)
-      }
+      this.appendTagsToListOfTags(filteredTags)
+      this.appendOrReplaceTagsAutoComplete()
     }
     else {
-      if (document.querySelector("#tags-autocomplete")) {
-        document.querySelector("#tags-autocomplete").remove()
-      }
+      document.querySelector("#tags-autocomplete") ? this.removeDOMElement(document.querySelector("#tags-autocomplete")) : ''
     }
     
-    if (this.comboboxSearch.value === "") {
-      document.querySelector("#tags-autocomplete").remove()
-    }
+    this.comboboxSearch.value === "" ? this.removeDOMElement(document.querySelector("#tags-autocomplete")) : ''
   }
 
   async getTagsLits() {
@@ -60,25 +42,68 @@ export default class extends Controller {
         'X-CSRFToken': this.securityToken
       }
     })
-
     const response = await request.perform()
+    if (!response.ok) {  return [] }
+    return JSON.parse(await response.text).tags
+  }
 
-    if (response.ok) {
-      return JSON.parse(await response.text).tags
+  createAutoCopmpleteElement() {
+    tagsAutoComplete = document.createElement("div")
+    this.setElementAttributes(tagsAutoComplete, {id: "tags-autocomplete", class: ["bg-gv-dark-jungle", "p-2", "mt-2"] })
+  }
+
+  creatListOfTagsElement() {
+    listOfTags = document.createElement("ul")
+    this.setElementAttributes(listOfTags, {class: ["list-of-tags"] })
+  }
+
+  setElementAttributes(element, params) {
+    element.id = params?.id
+    params?.class.forEach((classItem) => {
+      element.classList.add(classItem)
+    })
+  }
+
+  appendTagsToListOfTags(filteredTags) {
+    for (let i = 0; i < filteredTags.length; i++) {
+      const li = `<li id='${filteredTags[i]}' class='border-b-2 border-b-gv-baltic-sea p-2'} data-controller='tags' data-action='click->tags#appendToCombobox'>
+        ${filteredTags[i]}
+      </li>`
+      listOfTags.insertAdjacentHTML("beforeend", li)
+    }
+    
+    tagsAutoComplete.appendChild(listOfTags)
+  }
+
+  appendOrReplaceTagsAutoComplete() {
+    // The #tags-autocomplete div is replace with the new tags as the user types
+    if (document.querySelector("#tags-autocomplete")) {
+      this.comboboxTag.replaceChild(tagsAutoComplete, document.querySelector("#tags-autocomplete"))
+    } else {
+      this.comboboxTag.appendChild(tagsAutoComplete)
     }
   }
 
-  appendToCombobox() {
-    const li = `<li id='${this.element.textContent.trim().toLowerCase().split(' ').join('_')}' class='tag-item flex flex-row justify-between gap-4 items-center'>
-      ${this.element.textContent}
+  appendToCombobox(value) {
+    const li = `<li id='${value ? value : this.element.textContent.trim().toLowerCase().split(' ').join('_')}' class='tag-item flex flex-row justify-between gap-4 items-center'>
+      ${value ? value : this.element.textContent}
       ${this.closeSpan}
     </li>`
     this.tagsSelected.insertAdjacentHTML("beforeend", li)
     this.comboboxSearch.focus()
   }
+
+  registerTagToCombobox(event) {
+    event.preventDefault()
+    this.appendToCombobox(this.element.value)
+  }
   
-  removeTag() {
+  removeTagItem() {
     this.element.parentElement.remove()
     this.comboboxSearch.focus()
+  }
+
+  removeDOMElement(tag) {
+    tag.remove()
   }
 }
