67d1694d254f7a49813ca3a381c4353d9d0909c0
MauricioNC <mauricionc19@gmail.com>
2024-02-05 21:45:02 -0600

    feat: Added new field 'confirmed_session' to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_03_031737) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_06_031922) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,6 +92,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_03_031737) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "confirmed_session", default: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
