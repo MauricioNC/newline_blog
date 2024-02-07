c349d606ae56532f562169771fd1e52b31fdd27a
MauricioNC <mauricionc19@gmail.com>
2024-02-07 12:51:33 -0600

    feat: Updated the sessions controller to generate a new token for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,27 @@
 class SessionsController < ApplicationController
   before_action :user_authenticated?, only: [ :new ]
+  before_action :set_user, only: [:create]
 
   def new
     @user = User.new
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      flash[:notice] = "Email or password incorrect"
-      render :new
+    token = generate_token @user
+    UserMailer.with(user: @user, token: token).signin_confirmation.deliver_later
+    respond_to do |format|
+      format.turbo_stream {}
     end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by_email(params[:email])
   end
 end
