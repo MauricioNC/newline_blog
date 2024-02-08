55fdb421029590044fe1d955be8752dd5569cac9
MauricioNC <mauricionc19@gmail.com>
2024-02-07 23:07:34 -0600

    feat: Separated logic in the accound controller

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,10 +1,7 @@
 class AccountController < ApplicationController
+  before_action :decode_token
+
   def confirmation
-    redirect_to login_path, status: :unauthorized if params[:token].nil?
-
-    token = params[:token]
-    @decoded = JwtTokenService.decode(token)
-
     raise JWT::DecodeError, @decoded if @decoded.instance_of?(String)
 
     session[:user_id] = User.find(@decoded[:user_id]).id
@@ -14,5 +11,14 @@ class AccountController < ApplicationController
     redirect_to login_path, notice: e.message
   rescue JWT::DecodeError => e
     redirect_to login_path, notice: e.message
+  end
+
+  private
+
+  def decode_token
+    redirect_to login_path, status: :unauthorized if params[:token].nil?
+
+    token = params[:token]
+    @decoded = JwtTokenService.decode(token)
   end
 end
