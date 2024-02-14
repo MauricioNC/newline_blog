10f9eec5d38bc5025cc247054594c32924e00523
MauricioNC <mauricionc19@gmail.com>
2024-02-13 20:52:59 -0600

    Search functionality
    Was changed the whole logic to search a post to search a post
    using turbo frames, this means, make a 'live search'.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,25 @@ class HomeController < ApplicationController
 
   def search
     @query = params[:q]
-    @posts = Post.all.map{ |post| post if post.tags.select { |t| t === params[:q] } }
+    @posts = Post.where('lower(title) LIKE ?', "%#{@query.downcase}%").to_a
+    @posts_by_tag = Post.joins(:tags).where('lower(tags.tag) LIKE ?', "%#{@query.downcase}%").to_a
 
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.turbo_stream
+    @results =  unless @posts.empty? && @posts_by_tag.empty?
+                  @posts + @posts_by_tag
+                else
+                  []
+                end
+
+    @results.uniq.flatten.compact_blank! unless @results.nil?
+
+    if turbo_frame_request?
+      if params[:q].empty?
+        redirect_to root_path
+      else
+        render partial: "shared/cards", locals: { posts: @results, query: @query }
+      end
+    else
+      redirect_to root_path
     end
   end
 end
