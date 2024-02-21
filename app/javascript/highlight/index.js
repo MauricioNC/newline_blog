c9abada05e6c79f98a87439719b99252243b1b51
MauricioNC <mauricionc19@gmail.com>
2024-02-21 13:39:13 -0600

    feat: Added styles when show post and highlight code

--- app/javascript/highlight/index.js
+++ app/javascript/highlight/index.js
@@ -1,7 +1,8 @@
-import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/es/highlight.min.js';
+// import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/es/highlight.min.js';
+import { HighlightJS } from "highlight.js"
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('turbo:load', (event) => {
   document.querySelectorAll('pre code').forEach((el) => {
-    hljs.highlightElement(el);
+    HighlightJS.highlightElement(el);
   });
 });
